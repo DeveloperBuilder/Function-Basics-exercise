84b2226f8b8651b85bb38280f17073abbd041cd3
DeveloperBuilder <35078547+DeveloperBuilder@users.noreply.github.com>
2021-10-07 22:36:05 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const makePizza = function (){
+ console.log("Pizzadeeg maken");
+ console.log("Voeg topping op je pizza");
+ console.log("Pizza in de oven doen");
+ };
+
+ const makeSushi = function(){
+ console.log("Maak sushirijst");
+ console.log("Maak een sushirol");
+ console.log("Snijd sushirol in 6 stukken");
+ };
+
+ makeSushi();
+ makeSushi();
+ makeSushi();
+ makePizza();
+ makePizza();
